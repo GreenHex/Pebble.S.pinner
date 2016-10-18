a35ac49d7a3b0240029faaf1e76e73305a4ddfa0
GreenHex <GreenHex@gmail.com>
2016-10-18 18:24:01 +0530

    Finally getting info from weatherunderground.com

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -4,14 +4,46 @@ var GEOLOOKUP_URL = "http://api.wunderground.com/api/" + WU_API_KEY + "/geolooku
 var ASTRONOMY_URL = "http://api.wunderground.com/api/" + WU_API_KEY + "/astronomy/q/{country}/{state}/{city}.json";
 var DEBUG = 1;
 
-var xhrRequest = function ( url, type, callback ) {
+function play() {
+  var json;
+  try {
+    json = JSON.parse( this.responseText );
+    if (DEBUG) console.log( JSON.stringify( json ) );
+  } catch ( err ) {
+    if (DEBUG) console.log( 'index.js: play(): Error parsing responseText, invalid JSON data.' );
+    return;
+  }
+}
+
+function getAstroInfo() {
+  var json;
+  try {
+    if (DEBUG) json = JSON.parse( this.responseText );
+    console.log( JSON.stringify( json ) );
+    if (DEBUG) console.log( json.location.country_name + " " + json.location.state + " " + json.location.city );
+  } catch ( err ) {
+    if (DEBUG) console.log( 'index.js: getAstroInfo(): Error parsing responseText, invalid JSON data.' );
+    return;
+  }
+  var url = ASTRONOMY_URL.replace( "{country}", json.location.country_name ).replace( "{state}", json.location.state ).replace( "{city}", json.location.city );
+  if (DEBUG) console.log( url );
   var xhr = new XMLHttpRequest();
-  xhr.onload = function () {
-    callback( this.responseText );
-  };
-  xhr.open( type, url );
+  xhr.onload = play;
+  xhr.open( 'GET', url );
   xhr.send();
-};
+}
+
+function getGeoInfo( pos ) {
+  if (DEBUG) console.log( "index.js: Got location: " + JSON.stringify( pos ) );
+  
+  var url = GEOLOOKUP_URL.replace( "{lat}", pos.coords.latitude ).replace( "{lon}", pos.coords.longitude );
+  console.log( url );
+  
+  var xhr = new XMLHttpRequest();
+  xhr.onload = getAstroInfo;
+  xhr.open( 'GET', url );
+  xhr.send();
+}
 
 function getLocation() {
   if (DEBUG) console.log( "index.js: getLocation()." );
@@ -21,37 +53,6 @@ function getLocation() {
     locationError,
     { timeout: 15000, maximumAge: 60000 }
   );
-}
-
-function getGeoInfo( pos ) {
-  if (DEBUG) console.log( "index.js: Got location: " + JSON.stringify( pos ) );
-  var json;
-  
-  var url = GEOLOOKUP_URL.replace( "{lat}", pos.coords.latitude ).replace( "{lon}", pos.coords.longitude );
-  console.log( url );
-  new xhrRequest( url, 'POST',
-             function( responseText ) {
-               try {
-                 json = JSON.parse( responseText );
-                 console.log( JSON.stringify( json ) );
-                 console.log( json.location.country_name + " " + json.location.state + " " + json.location.city );
-               } catch ( err ) {
-                 if (DEBUG) console.log( 'index.js: xhrRequest(): Error parsing responseText, invalid JSON data.' );
-                 return;
-               }
-               var url = ASTRONOMY_URL.replace( "{country}", json.location.country_name ).replace( "{state}", json.location.state ).replace( "{city}", json.location.city );
-               console.log( url );
-               new xhrRequest( url, 'POST',
-                          function( responseText ) {
-                            try {
-                              json = JSON.parse( responseText );
-                            } catch ( err ) {
-                              if (DEBUG) console.log( 'index.js: xhrRequest(): Error parsing responseText, invalid JSON data.' );
-                              return;
-                            }
-                          });
-               console.log( JSON.stringify( json ) );
-             });
 }
 
 function locationError( err ) {
